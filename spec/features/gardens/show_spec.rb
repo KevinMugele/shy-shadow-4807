743dfeba5993278bcfd57eb81c75d496292735e8
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-22 12:46:15 -0400

    Write tests for user stories 3 and 4

---
+++ spec/features/gardens/show_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe 'Garden Show Page' do
+  before :each do
+    @garden1 = Garden.create!(name: "Flower Garden", organic: true)
+    @garden2 = Garden.create!(name: "Henry's Farm", organic: false)
+    @garden3 = Garden.create!(name: "Bee Farm", organic: true)
+    @plot1 = @garden1.plots.create!(number: 1, size: "Large", direction: "East")
+    @plot2 = @garden1.plots.create!(number: 2, size: "Large", direction: "East")
+    @plot3 = @garden2.plots.create!(number: 3, size: "Large", direction: "East")
+    @plot4 = @garden2.plots.create!(number: 4, size: "Large", direction: "East")
+    @plot5 = @garden3.plots.create!(number: 5, size: "Large", direction: "East")
+    @plot6 = @garden3.plots.create!(number: 6, size: "Large", direction: "East")
+    @plot7 = @garden1.plots.create!(number: 6, size: "Large", direction: "East")
+    @plant1 = Plant.create!(name: "Rose", description: 'flower', days_to_harvest: 5)
+    @plant2 = Plant.create!(name: "Bud", description: 'weed', days_to_harvest: 5000)
+    @plant3 = Plant.create!(name: "Tomato", description: 'red', days_to_harvest: 5)
+    @plant4 = Plant.create!(name: "Pepper", description: 'green', days_to_harvest: 5)
+    @plant5 = Plant.create!(name: "Zuchini", description: 'yummy', days_to_harvest: 5)
+    @plant6 = Plant.create!(name: "Corn", description: 'sweet', days_to_harvest: 5)
+    @plant7 = Plant.create!(name: "Carrot", description: 'cool', days_to_harvest: 5)
+    @pp1 = PlotPlant.create!(plot_id: @plot1.id, plant_id: @plant1.id)
+    @pp2 = PlotPlant.create!(plot_id: @plot2.id, plant_id: @plant1.id)
+    @pp3 = PlotPlant.create!(plot_id: @plot3.id, plant_id: @plant3.id)
+    @pp4 = PlotPlant.create!(plot_id: @plot4.id, plant_id: @plant4.id)
+    @pp5 = PlotPlant.create!(plot_id: @plot5.id, plant_id: @plant5.id)
+    @pp6 = PlotPlant.create!(plot_id: @plot6.id, plant_id: @plant6.id)
+    @pp7 = PlotPlant.create!(plot_id: @plot1.id, plant_id: @plant7.id)
+  end
+
+  describe 'user story 3' do
+    it 'lists all plants that have less than 100 days harvest time for garden' do
+      visit garden_path(@garden1)
+
+      expect(page).to have_content(@plant1.name)
+      expect(page).to have_content(@plant7.name)
+    end
+
+    describe 'user story 4' do
+      it 'sorted by most number of plants' do
+        visit garden_path(@garden1)
+
+        expect(@plant1.name).to appear_before(@plant7.name)
+      end
+    end
+  end
+end
