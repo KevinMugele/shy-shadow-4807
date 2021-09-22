4b4692c0778f75c21172f07414c5e897723106e4
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-22 11:39:25 -0400

    Updated route to use resources, including nested resource for destroying plantplot

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/plots', to: 'plots#index'
+  resources :plots, only: :index do
+    resources :plants, only: :destroy
+  end
 end
