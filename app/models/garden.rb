c1d4e219afb93502270fe709c5b7fff203ddf2e2
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-22 12:46:40 -0400

    Write method for finding plants for specific gardens and sorting by harvest time

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,12 @@
 class Garden < ApplicationRecord
   has_many :plots
+
+  def plants_by_harvest_time(days = 100)
+    plots.joins(plot_plants: :plant)
+         .select('plants.*, count(plot_plants.id) AS plant_count')
+         .where('plants.days_to_harvest < ?', days)
+         .group('plants.id')
+         .order('plant_count desc')
+         .distinct
+  end
 end
